71a9da8996ea136499d4f5155679d4c86208365d
LeandroAlmeida1999 <76185581+LeandroAlmeida1999@users.noreply.github.com>
2021-04-05 01:57:08 -0300

    primeiro commit

---
+++ Conta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta {get; set;}
+        private double Saldo {get; set;}
+        private double Credito{get; set;}
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string Nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = Nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            //validação de saldo suficiente
+            if(this.Saldo - valorSaque < (this.Credito*-1)){
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            // https://docs.microsoft.com/pt-br/dotnet/standard/base-types/composite-formatting
+
+            return true;
+        }
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta {0} é {1}", this.Nome, this.Saldo);
+        }
+        public void Transferir(double valorTransferencia, Conta ContaDestino)
+        {
+            if (this.Sacar(valorTransferencia)){
+                ContaDestino.Depositar(valorTransferencia);
+            }
+        }
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta" + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += " Crédito " + this.Credito;
+            return retorno;
+        }
+    }
+}
